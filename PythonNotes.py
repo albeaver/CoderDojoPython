3e3cc288c76d910af07d6a905c7f48e31b5e2641
albeaver <albeaver@users.noreply.github.com>
2017-01-15 11:02:15 -0800

    Add files via upload

---
+++ PythonNotes.py
@@ -0,0 +1,93 @@
+#PYTHON DOCS
+
+#Get everyone on Python BEFORE 7:10
+
+#############################
+#    DO THE FIRST PART IN THE CLI   #
+#############################
+
+
+#Printing
+#Printing is the most important part of Python
+#It lets you give the user information
+#Start with Hello World (explain why it is important)
+
+print("Hello World")
+
+#Variables
+#Ask if anyone knows what variables are
+#Use Bucket Analogy
+
+myFirstVariable = 0
+#Ask what will happen if you print myVariable
+
+#Types of variables
+
+#Int, Float, String, Boolean
+
+#Create profile
+
+myName = "John Smith"
+myAge = 18
+myAddress = "123 A st. NY"
+
+#print profile
+
+print(myName)
+print(myAge)
+print(myAddress)
+
+###########################
+#    CREATE A NEW DOCUMENT    #
+###########################
+
+#COMPARISONS
+myComparisonVariable = 0
+
+if myComparisonVariable == 0:
+    print("0")
+elif myComparisonVariable > 0:
+    print("Greater than 0")
+else:
+    print("Less than ZERO")
+
+####RUN####
+#Change myComparisonVariable to 1
+##Rerun
+#Change to -1
+##Rerun
+
+#Booleans are either True or False. An if statement tests if a condition is True or False. Therefore, the if statement is technically a Boolean
+
+
+#Operators
+    #   <   >   <=  >=  ==  !=
+
+#REAL LIFE SITUATION
+
+#In 1998, the U.S. Government passed COPPA, a law that restricts online accounts to people over the age of 13 or older. You are a developer for a website. See if a user meets the COPPA requirements.
+
+#allocate a couple of min. for trying out
+
+#INPUT
+
+#Explain that input is how a user interacts with the application.
+
+#####           #                   #######
+#                    #                          #
+#                   #                           #
+#                   #                           #
+####            # ###           #######
+
+myInput = input("Enter a string")
+print(myInput)
+
+#You can also have integer inputs
+
+myIntegerInput = int(input("Enter an integer"))
+myFinalInteger = myIntegerInput + 1 #THIS USES MATH WHICH HASN'T BEEN COVERED YET
+print(myFinalInteger)
+
+
+#CHALLENGES
+
